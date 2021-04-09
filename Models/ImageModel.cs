9c46975df353e82a623ab258b93051a6d2a9e742
Audrey <audreymandy096@gmail.com>
2021-04-08 18:22:26 -0600

    Changes

---
+++ Models/ImageModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+
+namespace OperationDigger.Models
+{
+    public class ImageModel
+    {
+        [Key]
+        public int ImageId { get; set; }
+
+        [Column(TypeName="nvarchar(50)")]
+        public string ImageName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Image Name")]
+        public string ImageTitle { get; set; }
+
+        [NotMapped]
+        [DisplayName("Upload Your Image")]
+        public IFormFile ImageFile { get; set; }
+    }
+}

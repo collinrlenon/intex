c936acbeac1cd6e5e1ceaad3dc4a0bdc4ced5004
collinrlenon <61851723+collinrlenon@users.noreply.github.com>
2021-04-10 21:48:09 -0400

    Added functionality to the Controller

--- Models/Burial.cs
+++ Models/Burial.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -7,6 +8,7 @@ namespace OperationDigger.Models
 {
     public partial class Burial
     {
+        [Key]
         public int BurialId { get; set; }
         public string LocationId { get; set; }
         public string BurialNs { get; set; }
